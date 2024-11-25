9a257534cd46895d273da02b386feb90dda85e2d
sbraende <sebastian.braende@gmail.com>
2024-11-25 11:34:33 +0100

    Implemented store feature

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,12 @@
 const form = document.querySelector(".contact-app__form");
 const firstnameInput = form.querySelector("[name='firstname']");
 const lastnameInput = form.querySelector("[name='lastname']");
-const phoneInput = form.querySelector("[name='phone']");
+const phoneInput = form.querySelector("[name='phone-number']");
 const addressInput = form.querySelector("[name='address']");
 const submitButton = form.querySelector(".form__submit-button");
 
 // DECLARING VARIABLES
-const contacts = [];
+const contacts = JSON.parse(localStorage.getItem("contacts")) || []; // ifs true - start with contacts (parsed data). false it will create [].
 
 const addContacts = (e) => {
   e.preventDefault();
@@ -19,7 +19,15 @@ const addContacts = (e) => {
     contactAddress: addressInput.value,
   };
   contacts.push(contact);
+  storeContacts(contacts);
   console.log(contacts);
 };
 
+const storeContacts = (contactsArray) => {
+  localStorage.setItem("contacts", JSON.stringify(contactsArray));
+};
+
+// ADD EVENT LISTENER TO THE FORM TO ADD CONTACTS
 form.addEventListener("submit", addContacts);
+
+console.log(contacts);
