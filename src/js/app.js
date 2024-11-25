01344056fe25b98566425847f92837f75644d65d
sbraende <sebastian.braende@gmail.com>
2024-11-25 11:16:56 +0100

    Implement add feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-console.log("sev");
+// SELECTING ELEMENTS
+const form = document.querySelector(".contact-app__form");
+const firstnameInput = form.querySelector("[name='firstname']");
+const lastnameInput = form.querySelector("[name='lastname']");
+const phoneInput = form.querySelector("[name='phone']");
+const addressInput = form.querySelector("[name='address']");
+const submitButton = form.querySelector(".form__submit-button");
+
+// DECLARING VARIABLES
+const contacts = [];
+
+const addContacts = (e) => {
+  e.preventDefault();
+  const contact = {
+    id: Date.now(),
+    contactFristname: firstnameInput.value,
+    contactLastname: lastnameInput.value,
+    contactPhone: phoneInput.value,
+    contactAddress: addressInput.value,
+  };
+  contacts.push(contact);
+  console.log(contacts);
+};
+
+form.addEventListener("submit", addContacts);
